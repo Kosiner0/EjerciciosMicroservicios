239102a089a736e39547394384be3511283aa61c
Kosiner0 <beniburdet17@gmail.com>
2024-05-31 13:01:53 +0200

    correcion ejercicio Curso sin terminar

---
+++ 03-CursosCRUD/src/main/java/com/curso/dao/CursoDao.java
@@ -0,0 +1,17 @@
+package com.curso.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.curso.model.Curso;
+
+public interface CursoDao extends JpaRepository<Curso, Integer> {
+
+	/*
+		@Query("SELECT c FROM Curso c WHERE c.precio BETWEEN :minPrecio AND :maxPrecio")
+		List<Curso> buscarPorPrecio(double minPrecio, double maxPrecio);
+	*/
+	
+	List<Curso> findByIdPrecioBetween(double minPrecio, double maxPrecio);
+}
--- Microservicio-Pedidos/src/main/java/com/curso/model/DOTProducto.java
+++ Microservicio-Pedidos/src/main/java/com/curso/model/DOTProducto.java
@@ -1,7 +1,15 @@
 package com.curso.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class DOTProducto {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int codProducto;
 	private String nombre;
 	private int stock;
